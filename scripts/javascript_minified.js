1142661c6a622c217a9ab4c483540e429a7d7702
MaraPeasley <mara.peasley@protonmail.com>
2023-06-06 14:34:46 -0700

    Fixed loading issues in Chrome
    Added "will-change: opacity;" to .study-content, .study-visual, and it's casestudy:hover elements.

--- scripts/javascript_minified.js
+++
@@ -1,5 +0,0 @@
-//Javasripts combined in this minified:
-//case_study_toggle.js
-//fadeinscroll.js
-
-function myFunction(){var e=document.getElementById("firstCaseStudy").src;"images/case_study_card_graphic.png"===e.innerHTML?e.innerHTML="images/case_study_description.svg":e.innerHTML="images/case_study_card_graphic.png"}let elementsArray=document.querySelectorAll(".tile");function fadeIn(){for(var e=0;e<elementsArray.length;e++){var n=elementsArray[e];n.getBoundingClientRect().top-window.innerHeight+20<0?n.classList.add("inView"):n.classList.remove("inView")}}console.log(elementsArray),window.addEventListener("scroll",fadeIn),fadeIn();
